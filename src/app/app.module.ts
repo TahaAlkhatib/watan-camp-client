e91aec0ef72f9df51b0283f6ee93a064b480eeb5
taha_mabn@hotmail.com <taha_mabn@hotmail.com>
2023-01-15 14:35:58 +0300

    chore: add upupa and material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { NgModule,Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { IonicModule } from '@ionic/angular';
@@ -13,6 +13,25 @@ import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
+import { LanguageModule, TranslationModule, LanguageService } from '@upupa/language';
+import { AuthModule, DEFAULT_SIGNIN, DEFAULT_VERIFY } from '@upupa/auth';
+import { ConfirmModule, EventBus, UtilsModule } from '@upupa/common';
+import { DataModule } from '@upupa/data';
+import { UploadModule } from '@upupa/upload';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const x = { DEFAULT_SIGNIN };
+const signinProvider: Provider = {
+    provide: DEFAULT_SIGNIN,
+    useFactory: (lang: LanguageService) => `/ar/account/signin`,
+    deps: [LanguageService],
+};
+const verifyProvider: Provider = {
+    provide: DEFAULT_VERIFY,
+    useFactory: (lang: LanguageService) => `/ar/account/verify`,
+    deps: [LanguageService],
+};
+
 @NgModule({
   imports: [
     BrowserModule,
@@ -23,7 +42,15 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     IonicStorageModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production
-    })
+    }),
+    UtilsModule,
+    ConfirmModule,
+    AuthModule.forRoot(`${environment.server_base_url}/auth`, null, signinProvider, null, verifyProvider),
+    DataModule.forChild(`${environment.server_base_url}/api`),
+    LanguageModule.forRoot('en', {}, 'lang', '/assets/langs'),
+    TranslationModule,
+    UploadModule.forChild(`${environment.server_base_url}/storage`),
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
