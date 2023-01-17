3039562fdcfcde570e7869150ad784a772128454
Zafer <smart.man08@gmail.com>
2023-01-17 03:40:42 +0300

    fix: change colors to primary color

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { AwarenessComponent } from "./awareness/awareness.component";
 import { BensComponent } from "./bens/bens.component";
 import { HealthCareComponent } from "./bens/health-care/health-care.component";
+import { LearningAndTrainingComponent } from "./bens/learning-and-training/learning-and-training.component";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./home.component";
 import { ReportsComponent } from "./reports/reports.component";
-
+const materials = [MatIconModule]
 @NgModule({
     imports: [
+        ...materials,
         HomeRoutingModule,
         CommonModule],
     declarations: [
@@ -16,7 +19,8 @@ import { ReportsComponent } from "./reports/reports.component";
         AwarenessComponent,
         ReportsComponent,
         HealthCareComponent,
-        BensComponent]
+        BensComponent,
+    LearningAndTrainingComponent]
 
 })
 export class HomeModule {
