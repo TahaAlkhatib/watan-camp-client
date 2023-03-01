e4460511a7d048fda6cc2c4277a3ae6749d45f63
taha_mabn@hotmail.com <taha_mabn@hotmail.com>
2023-03-01 04:04:01 +0300

    chore: upgrade material theme

--- src/app/choose-camp/choose-camp.component.ts
+++ src/app/choose-camp/choose-camp.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { MatDialogRef } from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import { Camp } from "../model";
 import { AppService } from "../providers/app.service";
--- src/app/choose-lanuage/choose-lanuage.component.ts
+++ src/app/choose-lanuage/choose-lanuage.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { MatDialogRef } from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import { LanguageService } from "@upupa/language";
 
