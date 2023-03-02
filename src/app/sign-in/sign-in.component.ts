c50d70c25d40cd9fbbeb672dbe47a58f6d21ade5
Zafer <smart.man08@gmail.com>
2023-03-02 05:42:47 +0300

    fix: sign-in and sign-up pages

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,10 +1,21 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-    selector:'sign-in',
-    templateUrl:'sign-in.component.html',
-    styleUrls:['sign-in.component.scss']
+  selector: "sign-in",
+  templateUrl: "sign-in.component.html",
+  styleUrls: ["sign-in.component.scss"],
 })
 export class SignInComponent {
-    
+  userName: string = "";
+  password: string = "";
+  constructor(private router: Router) {}
+  lang = localStorage.getItem("language") ?? "en";
+
+  navTo(route: string) {
+    this.router.navigate([this.lang, route]);
+  }
+  onSubmit() {
+    console.log(this.userName, this.password);
+  }
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,42 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-    selector:'sign-up',
-    templateUrl:'sign-up.component.html',
-    styleUrls:['sign-up.component.scss']
+  selector: "sign-up",
+  templateUrl: "sign-up.component.html",
+  styleUrls: ["sign-up.component.scss"],
 })
 export class SignUpComponent {
-    
+  form: {
+    selection: string;
+    fName: string;
+    lName: string;
+    phone: string;
+    department: string;
+    sn: string;
+    camp: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  } = {
+    selection: "",
+    fName: "",
+    lName: "",
+    camp: "",
+    confirmPassword: "",
+    department: "",
+    email: "",
+    password: "",
+    phone: "",
+    sn: "",
+  };
+  lang = localStorage.getItem("language") ?? "en";
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    console.log(this.form);
+  }
+  navTo(route: string) {
+    this.router.navigate([this.lang, route]);
+  }
 }
