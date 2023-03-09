1a50e6659c862179ec4c2631a8040d64027e83ff
Zafer <smart.man08@gmail.com>
2023-03-09 05:03:40 +0300

    fix: construct profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { LanguageService } from "@upupa/language";
 
 @Component({
     selector: 'profile',
@@ -7,14 +8,28 @@ import { Router } from "@angular/router";
     styleUrls: ['profile.component.scss']
 })
 export class ProfileComponent {
-
-    constructor(private router: Router) {
+    isLoggedIn:boolean= false
+    avatar: string
+    userName: string = 'default user'
+    email: string = 'default@default.com'
+    address: string = 'default address'
+    phone: string = '+90 xxx xxx xx xx'
+    role: string = "employee, ben, doner"
+    dateOfBirth: Date = new Date()
+    camp: string = 'default camp'
+    department:string = 'default'
+    constructor(private router: Router,private lang:LanguageService) {
 
     }
 
 
     async ngOnInit() {
-        
+        // const token = localStorage.getItem('token')
+        // if(token) this.isLoggedIn = true 
+
+    }
+    navToSginIn(){
+        this.router.navigateByUrl(`${this.lang.language}/sign-in`)
     }
 
 }
