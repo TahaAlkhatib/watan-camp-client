527634c07ab573d580e8c513b29c95f0f479f293
taha_mabn@hotmail.com <taha_mabn@hotmail.com>
2023-03-09 05:51:51 +0300

    chore: open location in map app

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -9,25 +9,29 @@ import { catchError, map } from 'rxjs/operators';
     styleUrls: ['map.component.scss']
 })
 export class MapComponent {
-  
+
     apiLoaded: Observable<boolean>;
 
-  constructor(httpClient: HttpClient) {
-    // this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE', 'callback')
-    //     .pipe(
-    //       map(() => true),
-    //       catchError(() => of(false)),
-    //     );
-  }
+    constructor(httpClient: HttpClient) {
+        // this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE', 'callback')
+        //     .pipe(
+        //       map(() => true),
+        //       catchError(() => of(false)),
+        //     );
+    }
 
-  center: google.maps.LatLngLiteral = {lat: 41, lng: 29};
-  zoom = 10;
-  markerOptions: google.maps.MarkerOptions = {draggable: false};
-  markerPositions: google.maps.LatLngLiteral[] = [{lat:41,lng:29}];
+    center: google.maps.LatLngLiteral = { lat: 41, lng: 29 };
+    zoom = 10;
+    markerOptions: google.maps.MarkerOptions = { draggable: false };
+    markerPositions: google.maps.LatLngLiteral[] = [{ lat: 41, lng: 29 }];
 
-  addMarker(event: google.maps.MapMouseEvent) {
-    // this.markerPositions.push(event.latLng.toJSON());
-    // console.log(this.markerPositions)
-  }
-    
+    addMarker(event: google.maps.MapMouseEvent) {
+        // this.markerPositions.push(event.latLng.toJSON());
+        // console.log(this.markerPositions)
+    }
+
+    goToLocation() {        
+        window.location.href = 'geo:41,29'
+    }
+
 }
