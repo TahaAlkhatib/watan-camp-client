0c67083cf868febd3dc441d4acd98e69a1be5b95
smart.man08@gmail.com <smart.man08@gmail.com>
2023-03-22 16:09:58 +0300

    fix: add arabic name and content to veiw page

--- src/app/model/content-items.ts
+++ src/app/model/content-items.ts
@@ -1,9 +1,11 @@
 export type ContentItem = {
     type: 'video' | 'pdf' | 'html'
     name?: string
+    arName?:string
     url?: string
     files?: any[]
     content?: string
+    arContent?: string
 }
 
 export type ContentItems = {
