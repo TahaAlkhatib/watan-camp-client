967afeb6f5a7047e4274564e0dfbd37ab817066f
taha_mabn@hotmail.com <taha_mabn@hotmail.com>
2023-04-04 17:43:45 +0300

    feat: xlsx library, export complaints to excel

--- src/app/admin-panel/complaint/complaint-list/complaint.component.ts
+++ src/app/admin-panel/complaint/complaint-list/complaint.component.ts
@@ -5,6 +5,8 @@ import { DataAdapter, DataService, ServerDataSource } from '@upupa/data';
 import { ColumnsDescriptor } from '@upupa/table';
 import { Complaint } from '../../../../app/model';
 import { AppService } from '../../../../app/providers/app.service';
+import * as XLSX from "xlsx";
+import { firstValueFrom } from 'rxjs';
 
 @Component({
     selector: 'app-complaint',
@@ -20,17 +22,18 @@ export class ComplaintListComponent implements OnInit {
     selection: Complaint[]
     adapter: DataAdapter
     columns: ColumnsDescriptor = {
-        fullName:1,subject: 1, email:1,phone:1
+        fullName: 1, subject: 1, email: 1, phone: 1
     }
     actions: ActionDescriptor[] = [
         { variant: 'icon', name: 'preview', icon: 'preview', menu: false },
         { variant: 'icon', name: 'edit', icon: 'edit', menu: true },
         { position: 'menu', name: 'delete', icon: 'delete_outline', text: 'Delete', menu: true },
         { position: 'bulk', name: 'delete', icon: 'delete_outline', text: 'Delete', bulk: true },
-        { position: 'header', name: 'create', icon: 'add_circle_outline', text: 'Create', bulk: true }
+        // { position: 'header', name: 'create', icon: 'add_circle_outline', text: 'Create', bulk: true },
+        { position: 'header', name: 'export', icon: 'article_outline', text: 'Export To Excel', bulk: true }
     ]
     ngOnInit(): void {
-        let source = new ServerDataSource(this.ds, '/complaint', ['_id','fullName','email','phone','subject'])
+        let source = new ServerDataSource(this.ds, '/complaint', ['_id', 'fullName', 'email', 'phone', 'subject'])
         this.adapter = new DataAdapter(source, '_id', 'fullName')
 
     }
@@ -56,13 +59,16 @@ export class ComplaintListComponent implements OnInit {
             case 'create':
                 this.router.navigateByUrl('en/admin/complaint/add-complaint')
                 break;
+            case 'export':
+                await this.exportToExcel()
+                break;
             case 'edit': this.router.navigateByUrl(`en/admin/complaint/edit-complaint/${x.data[0]._id}`); break;
             case 'delete':
                 const dialogData = { maxWidth: '320px', title: 'Delete', confirmText: 'Permanently' } as ConfirmOptions
                 if (await this.confirmService.openWarning(dialogData)) {
                     for (const item of x.data)
                         await this.ds.delete(`complaint/${item._id}`)
-                        await this.refresh()
+                    await this.refresh()
                 }
                 break;
             default:
@@ -72,6 +78,18 @@ export class ComplaintListComponent implements OnInit {
                 }, this)
                 break;
         }
+
+
+    }
+
+    async exportToExcel() {
+        let complaints: Complaint[] = await firstValueFrom(this.ds.get<Complaint[]>('complaint', { per_page: 9999999 }))
+
+        let ws = XLSX.utils.json_to_sheet(complaints)
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+        XLSX.writeFile(wb, 'complaints.xlsx')
 
 
     }
